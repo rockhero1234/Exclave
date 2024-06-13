35ffaf9a0409540597704533a57f77ec4964ab4b
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2024-06-13 20:56:00 +0800

    Upgrade AGP to 8.4.2

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 apply(from = "../repositories.gradle.kts")
 
 dependencies {
-    implementation("com.android.tools.build:gradle:8.4.1")
+    implementation("com.android.tools.build:gradle:8.4.2")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.0")
     implementation("cn.hutool:hutool-http:5.8.28")
     implementation("cn.hutool:hutool-crypto:5.8.28")
